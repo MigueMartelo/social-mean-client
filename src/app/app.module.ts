4953e22af5ca7e243c46dcb1190d053ed554dc49
Miguel Martelo <miguemartelo1031@gmail.com>
2018-01-22 21:18:48 -0500

    Register component and register template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { routing, appRoutingProviders } from './app.routing';
 
 // Cargar componentes
@@ -16,7 +18,8 @@ import { RegisterComponent } from './components/register/register.component';
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    FormsModule
   ],
   providers: [
     appRoutingProviders
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../../models/user';
 
 @Component ({
   selector: 'register',
@@ -7,12 +9,21 @@ import { Component, OnInit } from '@angular/core';
 
 export class RegisterComponent implements OnInit {
   public title:string;
+  public user: User;
 
-  constructor(){
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router
+  ){
     this.title = 'Registrate';
+    this.user = new User("", "", "", "", "", "", "ROLE_USER", "");
   }
 
   ngOnInit(){
     console.log('Componente de registro cargado...');
+  }
+
+  onSubmit(){
+    console.log(this.user);
   }
 }
