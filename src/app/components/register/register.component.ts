b6f5a82ceca15fc8e29dfba404776e7833ec00a7
Miguel Martelo <miguemartelo1031@gmail.com>
2018-01-22 22:02:26 -0500

    Register User

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component ({
   selector: 'register',
-  templateUrl: './register.component.html'
+  templateUrl: './register.component.html',
+  providers: [UserService]
 })
 
 export class RegisterComponent implements OnInit {
   public title:string;
   public user: User;
+  public status: string;
 
   constructor(
     private _route: ActivatedRoute,
-    private _router: Router
+    private _router: Router,
+    private _userService: UserService
   ){
     this.title = 'Registrate';
     this.user = new User("", "", "", "", "", "", "ROLE_USER", "");
@@ -23,7 +27,20 @@ export class RegisterComponent implements OnInit {
     console.log('Componente de registro cargado...');
   }
 
-  onSubmit(){
-    console.log(this.user);
+  onSubmit(form){
+    this._userService.register(this.user).subscribe(
+      res => {
+        if(res.user && res.user._id){
+          //console.log(response.user);
+          this.status = 'success';
+          form.reset();
+        }else{
+          this.status = 'error';
+        }
+      },
+      err => {
+        console.error(<any>err);
+      }
+    );
   }
 }
